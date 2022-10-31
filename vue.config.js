6bc86aab26b151e6971fae62daf518b5405d0ebd
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-11-01 03:24:18 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,6 @@ module.exports = defineConfig({
     publicPath: './',
     transpileDependencies: true, 
     devServer: {
-        proxy: {
-            "https://google.co.jp": {
-                target: "http://localhost:8081",
-            }
-        },
         host: 'localhost',
         port: 8081,
     },
