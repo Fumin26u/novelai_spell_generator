9500f167184a7f0b50aece78f4959cd3574d772f
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-11-01 02:48:22 +0900

    routerは使わなそうなので一旦放置

--- src/router.js
+++ src/router.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
-import TestComponent from './TestComponent.vue'
 const routes = [
     {path: '/App', component: App},
-    {path: '/TestComponent', component: TestComponent},
 ]
 
 const router = createRouter({
