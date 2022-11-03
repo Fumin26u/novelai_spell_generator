929052535e09d5da83589242f825fc20dab05925
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-11-03 21:24:02 +0900

    Update SavedPrompt.vue

--- src/SavedPrompt.vue
+++ src/SavedPrompt.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        <p>test</p>
+    <div class="main">
+        <HeaderComponent></HeaderComponent> 
     </div>
 </template>
 <script>
-export default {
-    setup() {
+import HeaderComponent from './components/HeaderComponent.vue'
+import './assets/style.scss'
+import { onMounted } from 'vue'
+import axios from 'axios'
 
+export default {
+    components: {
+        HeaderComponent,
+    },
+    setup() {
+        // 画面ロード時、APIからログインユーザーの登録プロンプト一覧を取得
+        onMounted(async() => {
+            const url = '/register/api/getPreset.php'
+            await axios.get(url)
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
+        })
     }
 }
 </script>
