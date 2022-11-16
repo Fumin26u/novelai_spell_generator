fdd2230f339cdd6bea08d2a2279107b1f37a92b9
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-11-17 03:09:29 +0900

    Update header.php

--- register/header.php
+++ register/header.php
@@ -7,7 +7,7 @@
         <a href="<?= $home ?>t/privacy_policy.php">プライバシーポリシー</a>
     </div>
     <div class="header-right">
-            <a href="http://localhost:8080/" target="_blank">プロンプトジェネレーター</a>
+            <a href="https://nai-pg.com/" target="_blank">プロンプトジェネレーター</a>
         <?php if (!isset($_SESSION['user_id'])) { ?>
             <a href="<?= $home ?>login.php">ログイン</a>
             <a href="<?= $home ?>register.php">アカウント登録</a>
