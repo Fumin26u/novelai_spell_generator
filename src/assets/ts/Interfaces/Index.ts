772713afc2b572e380b3ac78c8b06242e8492df8
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-11-29 18:00:23 +0900

    オブジェクトの型定義を作成4 (DB登録コンポーネント内)
    optionsはどうしようもなかったので一旦anyにした 要対応

--- src/assets/ts/Interfaces/Index.ts
+++ src/assets/ts/Interfaces/Index.ts
@@ -35,3 +35,25 @@ export interface PromptList {
     show_all: boolean,
     content: Prompt[],
 }
+
+// Preset関連のInterface(DB用)
+export interface Preset {
+    preset_id: number | null,
+    image: string | ArrayBuffer | null,
+    imagePath: string | null,
+    from: string,
+    commands: string | null,
+    commands_ban: string | null,
+    description: string | null,
+    nsfw: Nsfw,
+    seed: string,
+    resolution_width: number | null,
+    resolution_height: number | null,
+    resolution: string | null,
+    model: string | null,
+    sampling: number | null,
+    sampling_algo: string | null,
+    scale: number | null,
+    options: any,
+    others: string,
+}
