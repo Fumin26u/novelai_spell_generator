37cfb28c811267160912a60263c6e2e7903c293e
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-01 12:50:21 +0900

    eslintの設定を.eslintrc.jsに移設して使えるようにする
    tsconfig.jsonのincludeプロパティに".eslintrc.js"を追加、eslintrc.jsをプロジェクトフォルダ直下に入れるとできた

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "node": true,
+        "vue/setup-compiler-macros": true
+    },
+    "extends": [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/typescript"
+    ],
+    "parserOptions": {
+        "parser": "@typescript-eslint/parser"
+    },
+    "rules": {
+        "no-unused-vars": "off",
+        "vue/no-unused-vars": "off",
+        "vue/script-setup-uses-vars": "off",
+        "@typescript-eslint/no-unused-vars": [ "error" ]
+    }
+}
