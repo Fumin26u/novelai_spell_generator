7b56c6a2c22f8f099f24ab8890b82b2ee65e2a6f
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-02 20:38:22 +0900

    余りにも競合が発生するのでプロジェクトを新規で作って移設することにした

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
     presets: ['@vue/cli-plugin-babel/preset'],
+    env: {
+        test: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+                ['@babel/plugin-proposal-decorators', { legacy: true }],
+                '@babel/plugin-proposal-function-sent',
+                '@babel/plugin-proposal-export-namespace-from',
+                '@babel/plugin-proposal-numeric-separator',
+                '@babel/plugin-proposal-throw-expressions',
+            ],
+        },
+        production: {
+            plugins: ['transform-remove-console'],
+        },
+    },
 }
