8fb961b35a0545d13a66a53fc0433ccdff1858e5
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-02 21:11:38 +0900

    新規にプロジェクトを作成して基礎データ作成

--- register/api/controllers/DBControllers.php
+++
@@ -1,7 +0,0 @@
-<?php
-interface DBControllers {
-    public function get($searchQuery = '');
-    public function delete($id);
-    public function create($post);
-    public function update($post, $id);
-}
--- register/database/system-conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// データベース設定
-const DSN = 'mysql:host=localhost;dbname=spell_generator;charset=utf8mb4;';
-const DBUSER = 'root';
-const DBPASS = '';
-
-// デバッグモード
-const DEBUG = true;
