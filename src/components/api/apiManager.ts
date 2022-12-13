2546aec3d9e7023e429ea97338a288d1278b8c5b
Fumiya0719 <75408448+Fumiya0719@users.noreply.github.com>
2022-12-14 05:15:46 +0900

    API通信共通化のためのクラス・関数を作成

---
+++ src/components/api/apiManager.ts
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+class ApiManager {
+    // POST
+    async post(
+        url: string,
+        formData: any = {}
+    ): Promise<{ [key: string]: boolean }> {
+        return await axios
+            .post(url, formData)
+            .then(() => {
+                return {
+                    error: false,
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+                return {
+                    error: true,
+                }
+            })
+    }
+
+    // GET
+    async get(url: string, query: any = {}): Promise<any> {
+        return await axios
+            .get(url, {
+                params: query,
+            })
+            .then((response) => {
+                if (response.data === '') {
+                    return {
+                        error: true,
+                        content: 'データの取得に失敗しました。',
+                    }
+                }
+
+                return {
+                    error: false,
+                    content: response.data,
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+                return {
+                    error: true,
+                    content: null,
+                }
+            })
+    }
+}
